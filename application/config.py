87c02ed0028c5ca474fa04b6a64222aca83421aa
= <>
2021-04-30 23:08:50 +0500

    sqlite db added

--- application/config.py
+++ application/config.py
@@ -2,7 +2,8 @@ class Config:
     SECRET_KEY = '123456789'
     SQL_USER = 'root'
     SQL_PASS = 'root'
-    SQL_URI = 'mysql+pymysql://root:root@localhost/bookexchanger'
+    # SQL_URI = 'mysql+pymysql://root:root@localhost/bookexchanger'
+    SQL_URI = 'sqlite:///bookexchanger.db'
     SQLALCHEMY_DATABASE_URI = SQL_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = None
