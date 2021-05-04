89a1dcd68a3798ef7b5f0e466285c41cbbc91c52
= <>
2021-05-04 23:59:40 +0500

    notification being fixed

--- application/API/BusinessLogic/NotificationsBL.py
+++ application/API/BusinessLogic/NotificationsBL.py
@@ -17,12 +17,12 @@ class NotificationsBL(BusinessLogic):
                 " JSON_OBJECT('book_cover_image', book_to_be_sent.book_cover_image, " \
                 "'book_title', book_to_be_sent.book_title) as book_to_send " \
                 "FROM notifications LEFT JOIN users on users.user_id = notifications.user_id " \
-                "LEFT JOIN exchange on exchange.exchange_id = notifications.exchange_id AND exchange.is_exchange_confirmed = 0 AND exchange.is_exchange_declined = 0 " \
+                "LEFT JOIN exchange on exchange.exchange_id = notifications.exchange_id" \
                 "LEFT JOIN book as book_to_be_received on " \
                 "book_to_be_received.book_id = exchange.`book_to_be_received_id` " \
                 "LEFT JOIN book as book_to_be_sent on " \
                 "book_to_be_sent.book_id = exchange.`book_to_be_sent_id`" \
-                "WHERE to_be_notified_user_id ='" + str(user.user_id)+"'"
+                "WHERE to_be_notified_user_id ='" + str(user.user_id)+"' AND notifications.is_exchange_confirmed = 0 AND notifications.is_exchange_declined = 0"
 
         return super().get_by_custom_query("notification", query, isMany=True, isDump=True)
 
